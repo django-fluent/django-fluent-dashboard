0fc4490cbbd5c4c993507e70b5ff26b6964a786f
Diederik van der Boor <vdboor@edoburu.nl>
2011-05-12 10:18:34 +0200

    Add admin-tools version notice

--- ecms_dashboard/__init__.py
+++ ecms_dashboard/__init__.py
@@ -3,5 +3,7 @@ VERSION = (0, 1, 0)
 # Do some sane version checking
 import admin_tools
 
+# admin-tools 0.4.0 is the base API.
+# admin-tools 0.4.1 fixes static files support for Django 1.3.
 if tuple(admin_tools.VERSION.split('.')) < (0,4,0):
     raise ImportError("At least Django admin_tools 0.4.0 is required to run this application")
