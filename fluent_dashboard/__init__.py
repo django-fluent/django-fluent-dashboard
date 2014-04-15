7b0ed8313f724cf75f365787f98ba08877ae62b5
Diederik van der Boor <vdboor@edoburu.nl>
2014-04-15 18:46:44 +0200

    Bump version to 0.4a1

--- fluent_dashboard/__init__.py
+++ fluent_dashboard/__init__.py
@@ -1,10 +1,8 @@
 # following PEP 386
-__version__ = "0.3.6"
+__version__ = "0.4a1"
 
 # Do some sane version checking
 import admin_tools
 
-# admin-tools 0.4.0 is the base API.
-# admin-tools 0.4.1 fixes static files support for Django 1.3.
-if tuple(admin_tools.VERSION.split('.')) < (0,4,0):
-    raise ImportError("At least Django admin_tools 0.4.0 is required to run this application")
+if tuple(admin_tools.VERSION.split('.')) < (0,5,1):
+    raise ImportError("At least Django admin_tools 0.5.1 is required to run this application")
