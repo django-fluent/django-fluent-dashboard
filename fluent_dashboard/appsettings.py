7adc68ca6dbb8591b436cd48f9bb0d6d6f77bc4d
Diederik van der Boor <vdboor@edoburu.nl>
2015-01-19 20:39:57 +0100

    Fix icon for latest fluent-comments

--- fluent_dashboard/appsettings.py
+++ fluent_dashboard/appsettings.py
@@ -14,6 +14,7 @@ FLUENT_DASHBOARD_APP_ICONS = {
     'auth/user':  'system-users.png',
     'cms/page': 'internet-web-browser.png',
     'comments/comment': 'kde-telepathy.png', #'irc-voice.png',
+    'fluent_comments/fluentcomment': 'kde-telepathy.png', #'irc-voice.png',
     'dashboardmods/rssdashboardmodule': 'feed-subscribe.png',
     'fluent_blogs/entry': 'view-calendar-journal.png',
     'fluent_pages/pagelayout': 'view-choose.png',
