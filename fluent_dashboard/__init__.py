03ef491b493f7ca0045105b1d279c118fd2a3682
Diederik van der Boor <vdboor@edoburu.nl>
2015-04-08 15:39:26 +0200

    Remove old version checking, trust pip now

--- fluent_dashboard/__init__.py
+++ fluent_dashboard/__init__.py
@@ -1,8 +1,2 @@
 # following PEP 386
 __version__ = "0.4"
-
-# Do some sane version checking
-import admin_tools
-
-if tuple(admin_tools.VERSION.split('.')) < (0,5,1):
-    raise ImportError("At least Django admin_tools 0.5.1 is required to run this application")
